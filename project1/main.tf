527ce3e2fe8154cf17ab47f8379621edc10952a6
camsvt <camsvt@camsvt.com>
2019-02-05 15:21:53 +0000

    updated

--- project1/main.tf
+++ project1/main.tf
@@ -52,7 +52,7 @@ output "Name" {
 }
 
 output "External_IP" {
-  value = "${google_compute_instance.default.network_interface.0.assigned_nat_ip}"
+  value = "${google_compute_instance.default.network_interface.0.access_config.0.assigned_nat_ip}"
 }
 
 output "Internal_IP" {
